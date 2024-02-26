a0510d682b6c7862341a23d9c769a25f6cbde75e
drherrig <159749901+drherrig@users.noreply.github.com>
2024-02-25 22:18:51 -0600

    Add files via upload

---
+++ IncidentEditVM.cs
@@ -0,0 +1,16 @@
+﻿namespace SportsPro.Models
+{
+    public class IncidentEditVM
+    {
+        public List<Customer> customers { get; set; }
+        public List<Product> products { get; set; }
+        public List<Technician> technicianes { get; set; }
+
+        public Incident currentIncident { get; set; }
+        public string activeUse { get; set; }
+
+        public string CheckActiveUse(string c) =>
+            c.ToLower() == activeUse.ToLower() ? "Edit" : "Add";
+
+    }
+}
---
+++ IncidentListVM.cs
@@ -0,0 +1,13 @@
+﻿namespace SportsPro.Models
+{
+    public class IncidentListVM
+    {
+        public List<Incident> Incidents { get; set; }
+        public string ActiveFilter { get; set; } = "All";
+
+        public string CheckActiveFilter(string c) =>
+            c.ToLower() == ActiveFilter.ToLower() ? "All" : "";
+
+
+    }
+}
